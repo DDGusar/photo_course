01a4390e24420725e243e049d2cf0f83a0bad0a4
Daria Gusar <pretzel1104@yandex.ru>
2022-04-28 00:16:34 +0300

    до pixelperfect

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+const menuButton = document.querySelector(".header__burger");
+const navMenu = document.querySelector(".header__menu");
+const logoMenu = document.querySelector(".header__logo");
+const formButton = document.querySelector(".register__button");
+const formEmail = document.querySelector(".register__email");
+
+menuButton.addEventListener("click", function (evt) {
+  menuButton.classList.toggle("header__burger_active");
+  logoMenu.classList.toggle("header__logo_hidden");
+  navMenu.classList.toggle("header__menu_open");
+});
+formButton.addEventListener("click", function (evt) {
+  formButton.innerHTML = "Спасибо!";
+});
+formEmail.addEventListener("click", function (evt) {
+  if (formEmail.value === "Ваша почта") {
+    formEmail.value = "";
+  }
+});
