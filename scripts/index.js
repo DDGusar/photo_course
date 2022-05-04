e634da824fe24526071391dcc266b299360c6145
Daria Gusar <pretzel1104@yandex.ru>
2022-05-04 14:18:11 +0300

    перед шлифовкой

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,13 @@ const navMenu = document.querySelector(".header__menu");
 const logoMenu = document.querySelector(".header__logo");
 const formButton = document.querySelector(".register__button");
 const formEmail = document.querySelector(".register__email");
+const menuHeader = document.querySelector(".header");
 
 menuButton.addEventListener("click", function (evt) {
   menuButton.classList.toggle("header__burger_active");
   logoMenu.classList.toggle("header__logo_hidden");
   navMenu.classList.toggle("header__menu_open");
+  menuHeader.classList.toggle("header_open");
 });
 formButton.addEventListener("click", function (evt) {
   formButton.innerHTML = "Спасибо!";
